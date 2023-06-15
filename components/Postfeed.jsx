02a4d9a1dc212e779d0b9a504f88fe5858cba685
unbeqem <111991086+unbeqem@users.noreply.github.com>
2023-06-15 15:30:56 +0200

    adding sidebar

---
+++ components/Postfeed.jsx
@@ -0,0 +1,13 @@
+import TweetInput from "./Tweetinput";
+
+export default function PostFeed() {
+    return (
+        <div className="sm:ml-20 xl:ml-96 max-w-2xl flex-grow border-gray-700 border-x">
+            <div className="px-3 py-2 text-lg sm:text-xl font-bold
+            border-b border-gray-700 sticky top-0 z-50">
+                Home
+            </div>
+            <TweetInput />
+        </div>
+    )
+}
---
+++ components/Trending.jsx
@@ -0,0 +1,7 @@
+export default function Trending() {
+    return (
+        <div className="hidden lg:flex">
+            Trending
+            </div>
+    )
+}
